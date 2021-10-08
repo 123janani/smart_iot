6e725c10e2b595ff0de4108dc5ec9977ea9ed6f4
123janani <dev1@eatmeglobal.com>
2021-10-08 09:37:21 +0530

    chaged folder

--- routes/sensor.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const sensorController = require("../controllers/sensor");
-
-router.get("/", sensorController.getSensorData);
-
-module.exports = router;
